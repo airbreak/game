ad28d62e1c1134d12ccbdacfb01f1716597487fb
airbreak <954957945@qq.com>
2015-10-14 17:14:09 +0800

    uu

--- start.php
+++ start.php
@@ -110,7 +110,7 @@ $signPackage = $jssdk->GetSignPackage();
     signature: '<?php echo $signPackage["signature"];?>',
     jsApiList: [
       // 所有要调用的 API 都要加到这个列表中
-        'checkJsApi','chooseImage','onMenuShareAppMessage','onMenuShareQQ','onMenuShareWeibo','onMenuShareQZone'
+        'checkJsApi','chooseImage','onMenuShareAppMessage','onMenuShareQQ','onMenuShareWeibo','onMenuShareQZone','onMenuShareTimeline'
     ]
   });
   wx.ready(function () {
